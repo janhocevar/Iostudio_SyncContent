657ce95d02daa585c8043be8de895ecab91129cc
Joshua Estes <Joshua.Estes@ScenicCityLabs.com>
2011-12-20 11:16:55 -0600

    initial commit

---
+++ app/code/community/Iostudio/SyncContent/Helper/Data.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Description
+ *
+ * @author
+ * @copyright
+ * @package
+ * @subpackage
+ * @version
+ *
+ */
+class Iostudio_SyncContent_Helper_Data extends Mage_Core_Helper_Abstract
+{
+  
+}
